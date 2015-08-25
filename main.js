df9afa6a2fdae44f58ad4d01870f885295075d00
Dan Flettre <fletd01@yahoo.com>
2015-08-25 16:03:22 -0500

    add format button, remove rule funk

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-var eslint = require('./eslint/lib/eslint.js')
-var config = require('eslint-config-standard')
 var ace = require('brace')
+var eslint = require('./eslint/lib/eslint.js')
+var standardCfg = require('eslint-config-standard')
+var standardFormat = require('standard-format')
+var reactCfg = require('eslint-config-standard-react')
+var extend = require('xtend')
 require('brace/mode/javascript')
 require('brace/theme/monokai')
 
-// remap custom rules
-config.rules['object-curly-even-spacing'] = config.rules['standard/object-curly-even-spacing']
-config.rules['array-bracket-even-spacing'] = config.rules['standard/array-bracket-even-spacing']
-config.rules['computed-property-even-spacing'] = config.rules['standard/computed-property-even-spacing']
-delete config.rules['standard/object-curly-even-spacing']
-delete config.rules['standard/array-bracket-even-spacing']
-delete config.rules['standard/computed-property-even-spacing']
+var config = extend({}, standardCfg)
+// add react rules
+config.ecmaFeatures = extend(config.ecmaFeatures, reactCfg.ecmaFeatures)
+config.rules = extend(config.rules, reactCfg.rules)
 
 var editor = ace.edit('javascript-editor')
 editor.getSession().setMode('ace/mode/javascript')
 editor.setTheme('ace/theme/monokai')
 editor.session.setUseWorker(false)
-editor.setValue('var foo = ["" ]')
+editor.setValue([
+  'var foo = "hello";',
+  'console.log(foo);',
+  ''
+].join('\n'))
 
 var h = require('virtual-dom/h')
 var main = require('main-loop')
@@ -24,14 +28,25 @@ var loop = main({ n: 0 }, render, require('virtual-dom'))
 document.querySelector('#messages').appendChild(loop.target)
 
 function render (state) {
+  return h('div', [renderFormatButton(), renderMessages(state)])
+}
+
+function renderFormatButton () {
+  return h('button', {onclick: formatCode}, 'Format Code')
+}
+
+function formatCode () {
+  editor.setValue(standardFormat.transform(editor.getValue()))
+}
+
+function renderMessages (state) {
   if (!state.messages) return h('div')
 
   var renderedMessages = state.messages.map(function (m) {
     var formattedMessage = m.line + ':' + m.column + ' - ' + m.message + ' (' + m.ruleId + ')'
     return h('div', {className: 'message'}, formattedMessage)
   })
-
-  return h('div', renderedMessages)
+  return renderedMessages
 }
 
 editor.getSession().on('change', doStuff)
@@ -41,7 +56,6 @@ function doStuff () {
 
   var annotations = []
   messages.forEach(function (message) {
-    console.log(message)
     annotations.push(
       {
         row: message.line - 1, // must be 0 based
