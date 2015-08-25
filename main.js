3d7cecf6815c3e9e362168ace1e2c3b68d48467e
Dan Flettre <fletd01@yahoo.com>
2015-08-25 17:16:22 -0500

    fix initialState

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ editor.setValue([
 
 var h = require('virtual-dom/h')
 var main = require('main-loop')
-var loop = main({ n: 0 }, render, require('virtual-dom'))
+var loop = main({messages: []}, render, require('virtual-dom'))
 document.querySelector('#messages').appendChild(loop.target)
 
 function render (state) {
